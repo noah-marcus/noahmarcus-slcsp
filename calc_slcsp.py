da1132f1fa99fe427cf211f775558959a4dfe751
Noah Marcus <Walcott@Noahs-MBP.home>
2022-08-30 20:49:26 -0400

    parse plans csv and generate silver_plans_rates dictionary

--- calc_slcsp.py
+++ calc_slcsp.py
@@ -2,11 +2,82 @@ import argparse
 import os.path
 import logging
 
+def parse_plans_csv(plans_csv_file):
+    """
+    Parse plans csv file and generate silver_plans_rates dictionary.
+
+    Parameters:
+    plans_csv_file : str
+        Filename given by user for csv file that contains all health plans in the U.S.
+
+    Returns:
+    silver_plans_rates_dict : dict
+        Dictionary of all silver rate plans found in csv file
+        key: tuple ('State', 'Rate Area')
+        values: { plan_id : str, lowest_rate : float, second_lowest_rate : float }
+    """
+    # Init return variable
+    silver_plans_rates_dict = {}
+
+    # Open file
+    with open(plans_csv_file) as plans_csv:
+        # Skip first line in csv file
+        next(plans_csv)
+
+        # Compile silver plans and rates into dictionary
+        for plan in plans_csv:
+            # Split plan line by comma
+            plan_params = plan.strip().split(',')
+
+            # Parse plan values
+            plan_id = plan_params[0]
+            state = plan_params[1]
+            metal_level = plan_params[2]
+            rate = float(plan_params[3])
+            rate_area = int(plan_params[4])
+
+            # If data is incomplete, skip entry
+            if not plan_id or not state or not metal_level or not rate or not rate_area:
+                continue
+
+            # Create rate area tuple from parameters
+            rate_area_tuple = (state, rate_area)
+
+            # Save data if silver plan
+            if metal_level.lower() == "silver":
+
+                # Add plan rate to dictionary
+                if rate_area_tuple in silver_plans_rates_dict:
+                    # Check if new rate is lowest or second lowest
+                    if rate < silver_plans_rates_dict[rate_area_tuple]['lowest_rate']:
+                        # If new rate is lowest, move previous lowest to second_lowest_rate
+                        silver_plans_rates_dict[rate_area_tuple]['second_lowest_rate'] = silver_plans_rates_dict[rate_area_tuple]['lowest_rate']
+                        # Set new lowest rate
+                        silver_plans_rates_dict[rate_area_tuple]['lowest_rate'] = rate
+
+                    elif silver_plans_rates_dict[rate_area_tuple]['second_lowest_rate'] == None \
+                        or rate < silver_plans_rates_dict[rate_area_tuple]['second_lowest_rate']:
+                        # If new rate is lower than the current second lowest rate, but higher than the current lowest rate
+                        # replace the current second lowest rate
+                        silver_plans_rates_dict[rate_area_tuple]['lowest_rate'] = rate
+
+
+                # Create entry in dictionary if it does not exist yet
+                else:
+                   silver_plans_rates_dict[rate_area_tuple] = {
+                        'lowest_rate': rate,
+                        'second_lowest_rate': None,
+                        'plan_id': plan_id
+                   }
+
+
+        return silver_plans_rates_dict
+
 def main():
     """
-    main() - Main function of calc_slcsp.py
+    Main function of calc_slcsp.py.
 
-    Description: Parse arguments and calculate the SLCSP of the given file.
+    Parse arguments and calculate the SLCSP of the given file.
     """
 
     # Initialize parser
@@ -34,6 +105,11 @@ def main():
         exit(1)
 
     logging.info("Files Validated\nInput given -- Plans CSV: {plans_csv_file}, Zip CSV: {zips_csv_file}, Input CSV: {input_csv_file}")
+
+    # Generate silver plan rates
+    silver_plans_rates_dict = parse_plans_csv(plans_csv_file)
+
+    print(silver_plans_rates_dict)
 
 if __name__ == "__main__":
     main()
